01192a2d9b7dcdfa14ef8bf0f7ebd72fc28bd1f2
Junyong Jung <thejunyongjung@gmail.com>
2026-05-22 16:53:46 -0700

    feat: scaffold Maven project with ui/models/util packages and Main banner

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        printBanner();
+        // TODO: Launch Homescreen (P11)
+    }
+
+    private static void printBanner() {
+        System.out.println();
+        System.out.println("Pizza-licious");
+    }
+}
+
